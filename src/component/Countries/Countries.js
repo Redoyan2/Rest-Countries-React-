2d538de8b8c9348492632d80099a9008b9a4c997
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-03-21 02:28:00 +0600

    fixed grid

--- src/component/Countries/Countries.js
+++ src/component/Countries/Countries.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Countries.css';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(countries=>setCountries(countries))
+
+    },[])
+
+ 
+
     return (
-        <div>
-            <h1>Show all the countries:</h1>
-            
+        <div className='HeadName'>
+            <h1>Show all the countries</h1>
+       <div className='Country-component'>
+       {
+            countries.map(country=> <Country country={country}></Country>)
+        }
+       </div>
+
         </div>
     );
 };
+
+function Country(prop){
+    const {name, flags, capital, population} = prop.country
+    return(
+            <div className='Country-div'>
+            <h5>Countries Name: <span>{ name.common}</span></h5>
+            <img src={flags.png} alt="" />
+            <h6>Capital Name: <span> {capital}</span></h6>
+            <h6>Populatjion: <span>{population}</span></h6>
+            
+            </div>
+    )
+}
 
 export default Countries;
